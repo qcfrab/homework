1f5f8b188ad526adae6506d54d3030d58a2532d8
Zhanna <ostrovskaazanna8@gmail.com>
2024-10-11 21:56:46 +0300

    Add hello file

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var N := ReadString('На каком языке вы говорите?');
+  case N of
+    'English': print('Hello!');
+    'Русский': print('Привет!');
+    'Italiano': print('Ciao!');
+    'Français': print('Bonjour!');
+  end;
+end.
